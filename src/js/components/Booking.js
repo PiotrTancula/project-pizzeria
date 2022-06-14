2868235b865236525c9ef9584117f9b42deaf4f8
Piotr Tańcula <tanculapiotr@gmail.com>
2022-06-14 19:10:30 +0200

    date and hour pick feature is present

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,11 +1,14 @@
 import { templates, select } from '../settings.js';
-import AmountWidget  from './AmountWidget.js';
+import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
   constructor(element) {
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
+
   }
 
   render(element) {
@@ -17,6 +20,9 @@ class Booking{
 
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+
+    thisBooking.dom.datePickerWrapper = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPickerWrapper = document.querySelector(select.widgets.hourPicker.wrapper);
 
   }
 
@@ -32,6 +38,10 @@ class Booking{
     thisBooking.dom.hoursAmount.addEventListener('click', function () {
 
     });
+
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePickerWrapper);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPickerWrapper);
+
 
   }
 }
